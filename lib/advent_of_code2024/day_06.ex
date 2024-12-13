5132e5a940410d04ce4c3de7557b2fa0b68b5491
Jason Schweier <jason.schweier@gmail.com>
2024-12-12 21:53:29 -0500

    WIP: Day 06 - part 2

--- lib/advent_of_code2024/day_06.ex
+++ lib/advent_of_code2024/day_06.ex
@@ -96,6 +96,100 @@ defmodule AdventOfCode2024.Day06 do
     In this example, the guard will visit 41 distinct positions on your map.
 
     Predict the path of the guard. How many distinct positions will the guard visit before leaving the mapped area?
+
+    --- Part Two ---
+
+    While The Historians begin working around the guard's patrol route, you borrow their fancy device and step outside the lab. From the safety of a supply closet, you time travel through the last few months and record the nightly status of the lab's guard post on the walls of the closet.
+
+    Returning after what seems like only a few seconds to The Historians, they explain that the guard's patrol area is simply too large for them to safely search the lab without getting caught.
+
+    Fortunately, they are pretty sure that adding a single new obstruction won't cause a time paradox. They'd like to place the new obstruction in such a way that the guard will get stuck in a loop, making the rest of the lab safe to search.
+
+    To have the lowest chance of creating a time paradox, The Historians would like to know all of the possible positions for such an obstruction. The new obstruction can't be placed at the guard's starting position - the guard is there right now and would notice.
+
+    In the above example, there are only 6 different positions where a new obstruction would cause the guard to get stuck in a loop. The diagrams of these six situations use O to mark the new obstruction, | to show a position where the guard moves up/down, - to show a position where the guard moves left/right, and + to show a position where the guard moves both up/down and left/right.
+
+    Option one, put a printing press next to the guard's starting position:
+
+    ....#.....
+    ....+---+#
+    ....|...|.
+    ..#.|...|.
+    ....|..#|.
+    ....|...|.
+    .#.O^---+.
+    ........#.
+    #.........
+    ......#...
+
+    Option two, put a stack of failed suit prototypes in the bottom right quadrant of the mapped area:
+
+    ....#.....
+    ....+---+#
+    ....|...|.
+    ..#.|...|.
+    ..+-+-+#|.
+    ..|.|.|.|.
+    .#+-^-+-+.
+    ......O.#.
+    #.........
+    ......#...
+
+    Option three, put a crate of chimney-squeeze prototype fabric next to the standing desk in the bottom right quadrant:
+
+    ....#.....
+    ....+---+#
+    ....|...|.
+    ..#.|...|.
+    ..+-+-+#|.
+    ..|.|.|.|.
+    .#+-^-+-+.
+    .+----+O#.
+    #+----+...
+    ......#...
+
+    Option four, put an alchemical retroencabulator near the bottom left corner:
+
+    ....#.....
+    ....+---+#
+    ....|...|.
+    ..#.|...|.
+    ..+-+-+#|.
+    ..|.|.|.|.
+    .#+-^-+-+.
+    ..|...|.#.
+    #O+---+...
+    ......#...
+
+    Option five, put the alchemical retroencabulator a bit to the right instead:
+
+    ....#.....
+    ....+---+#
+    ....|...|.
+    ..#.|...|.
+    ..+-+-+#|.
+    ..|.|.|.|.
+    .#+-^-+-+.
+    ....|.|.#.
+    #..O+-+...
+    ......#...
+
+    Option six, put a tank of sovereign glue right next to the tank of universal solvent:
+
+    ....#.....
+    ....+---+#
+    ....|...|.
+    ..#.|...|.
+    ..+-+-+#|.
+    ..|.|.|.|.
+    .#+-^-+-+.
+    .+----++#.
+    #+----++..
+    ......#O..
+
+    It doesn't really matter what you choose to use as an obstacle so long as you and The Historians can put it into position without the guard noticing. The important thing is having enough options that you can find one that minimizes time paradoxes, and in this example, there are 6 different positions you could choose.
+
+    You need to get the guard stuck in a loop by adding a single new obstruction. How many different positions could you choose for this obstruction?
   """
   def part1 do
     Inputs.lines(6, :binary)
@@ -104,7 +198,12 @@ defmodule AdventOfCode2024.Day06 do
     |> MapSet.size()
   end
 
+  # too low: 1473, 1474
   def part2 do
+    Inputs.lines(6, :binary)
+    |> parse()
+    |> obstacles_causing_loops()
+    |> MapSet.size()
   end
 
   def parse(map) do
@@ -121,14 +220,44 @@ defmodule AdventOfCode2024.Day06 do
   end
 
   def guard_visited(tiles) do
-    {coord, _} = start = Enum.find(tiles, &match?({_coord, {:guard, _}}, &1))
-
-    map =
-      tiles
-      |> Enum.into(Map.new())
-      |> Map.put(coord, :open)
+    {coord, _} = start = guard_starting_tile(tiles)
+    map = coordinate_map(tiles, [{coord, :open}])
 
     visit(start, map, MapSet.new())
+  end
+
+  def obstacles_causing_loops(tiles) do
+    {start_coordinate, _} = start = guard_starting_tile(tiles)
+    map = coordinate_map(tiles, [{start_coordinate, :open}])
+
+    tiles
+    |> guard_visited()
+    |> Enum.reject(&(&1 == start_coordinate))
+    |> Enum.filter(fn new_obstacle ->
+      new_map = Map.put(map, new_obstacle, :obstacle)
+
+      loop?(start, new_map, MapSet.new())
+    end)
+    |> Enum.into(MapSet.new())
+  end
+
+  def guard_loops?(tiles) do
+    {start_coordinate, _} = start = guard_starting_tile(tiles)
+    map = coordinate_map(tiles, [{start_coordinate, :open}])
+
+    loop?(start, map, MapSet.new())
+  end
+
+  defp guard_starting_tile(tiles) do
+    Enum.find(tiles, &match?({_coord, {:guard, _}}, &1))
+  end
+
+  defp coordinate_map(tiles, overrides) do
+    map = Enum.into(tiles, Map.new())
+
+    Enum.reduce(overrides, map, fn {coord, state}, acc ->
+      Map.put(acc, coord, state)
+    end)
   end
 
   defp tile_type("."), do: :open
@@ -155,6 +284,30 @@ defmodule AdventOfCode2024.Day06 do
 
       nil ->
         new_visited
+    end
+  end
+
+  defp loop?({coord, {:guard, dir}}, map, visited) do
+    if {coord, dir} in visited do
+      true
+    else
+      new_visited = MapSet.put(visited, {coord, dir})
+      new_coord = step_forward(dir, coord)
+      new_tile = Map.get(map, new_coord)
+
+      case new_tile do
+        :open ->
+          loop?({new_coord, {:guard, dir}}, map, new_visited)
+
+        :obstacle ->
+          new_dir = turn_right(dir)
+          new_coord = step_forward(new_dir, coord)
+
+          loop?({new_coord, {:guard, new_dir}}, map, new_visited)
+
+        nil ->
+          false
+      end
     end
   end
 
--- test/advent_of_code2024/day_06_test.exs
+++ test/advent_of_code2024/day_06_test.exs
@@ -50,4 +50,75 @@ defmodule AdventOfCode2024.Day06Test do
       assert MapSet.size(visited) == 41
     end
   end
+
+  describe "guard_loops?/2" do
+    test "simple loop" do
+      map =
+        """
+        ####
+        ...#
+        .^..
+        #...
+        ####
+        """
+        |> String.trim()
+        |> String.split("\n", trim: true)
+
+      assert guard_loops?(parse(map))
+    end
+
+    test "example" do
+      example = parse(@example)
+
+      refute guard_loops?(example)
+      assert guard_loops?(replace_tile(example, {6, 3}, :obstacle))
+      assert guard_loops?(replace_tile(example, {7, 6}, :obstacle))
+      assert guard_loops?(replace_tile(example, {7, 7}, :obstacle))
+      assert guard_loops?(replace_tile(example, {8, 1}, :obstacle))
+      assert guard_loops?(replace_tile(example, {8, 3}, :obstacle))
+      assert guard_loops?(replace_tile(example, {9, 7}, :obstacle))
+    end
+  end
+
+  describe "obstacles_causing_loops/1" do
+    test "simple loop" do
+      tiles =
+        as_tiles("""
+        ####
+        ....
+        .^..
+        #...
+        ####
+        """)
+
+      refute guard_loops?(tiles)
+      assert obstacles_causing_loops(tiles) == MapSet.new([{1, 3}])
+    end
+
+    test "example" do
+      assert obstacles_causing_loops(parse(@example)) ==
+               MapSet.new([
+                 {6, 3},
+                 {7, 6},
+                 {7, 7},
+                 {8, 1},
+                 {8, 3},
+                 {9, 7}
+               ])
+    end
+  end
+
+  defp as_tiles(map) do
+    map
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> parse()
+  end
+
+  def replace_tile(tiles, coordinate, new_value) do
+    Enum.map(tiles, fn
+      {^coordinate, _old_value} -> {coordinate, new_value}
+      otherwise -> otherwise
+    end)
+  end
 end
